cfc462bea99ad288762c5b435ff0e42fe09685a8
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2022-09-25 22:16:24 +0600

    table row widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/widgets/stream_builder_widget.dart';
+import 'package:flutter_application_1/widgets/table_row_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //     primarySwatch: Colors.blue,
       //     ),
-      home: StreamBuilderWidget(),
+      home: TableRowWidget(),
     );
   }
 }
---
+++ lib/widgets/table_row_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class TableRowWidget extends StatefulWidget {
+  const TableRowWidget({Key? key}) : super(key: key);
+
+  @override
+  _TableRowWidgetState createState() => _TableRowWidgetState();
+}
+
+class _TableRowWidgetState extends State<TableRowWidget> {
+  @override
+  Widget build(BuildContext context) {
+    TableRow tableRow = const TableRow(children: <Widget>[
+      Padding(
+        padding: EdgeInsets.all(10),
+        child: Text("Cell 1"),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10),
+        child: Text("Cell 2"),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10),
+        child: Text("Cell 3"),
+      )
+    ]);
+    return Scaffold(
+      body: Center(
+        child: Table(
+          border: TableBorder.all(),
+          defaultColumnWidth: const FixedColumnWidth(120),
+          children: <TableRow>[
+            tableRow,
+            tableRow,
+            tableRow,
+            tableRow,
+            tableRow
+          ],
+        ),
+      ),
+    );
+  }
+}
