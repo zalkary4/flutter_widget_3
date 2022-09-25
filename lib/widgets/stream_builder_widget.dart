5d86de2ae2594982b95141c91604e0bf210827f8
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2022-09-25 21:48:44 +0600

    stream builder

---
+++ lib/widgets/stream_builder_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class StreamBuilderWidget extends StatefulWidget {
+  const StreamBuilderWidget({Key? key}) : super(key: key);
+
+  @override
+  _StreamBuilderWidgetState createState() => _StreamBuilderWidgetState();
+}
+
+Stream<int> generateStream = (() async* {
+  await Future<void>.delayed(const Duration(seconds: 2));
+  yield 1;
+  await Future<void>.delayed(const Duration(seconds: 1));
+  yield 2;
+  throw 'An error ocured';
+  await Future<void>.delayed(const Duration(seconds: 1));
+  yield 3;
+})();
+
+class _StreamBuilderWidgetState extends State<StreamBuilderWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: StreamBuilder(
+          stream: generateStream,
+          initialData: 0,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator.adaptive();
+            }
+            if (snapshot.hasError) {
+              return const Text("error");
+            } else {
+              return Text(
+                snapshot.data.toString(),
+                style: const TextStyle(fontSize: 40),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
