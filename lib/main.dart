8ad000679d2c16039fd629b83b8d0fae47d365f5
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2022-09-28 19:46:56 +0600

    cupertino scrollbar widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/widgets/cupertino_popup_surface_widget.dart';
+import 'package:flutter_application_1/widgets/cupertino_scrollbar_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //     primarySwatch: Colors.blue,
       //     ),
-      home: CupertinoPopupSurfaceWidget(),
+      home: CupertinoScrollbarWidget(),
     );
   }
 }
---
+++ lib/widgets/cupertino_scrollbar_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+
+class CupertinoScrollbarWidget extends StatefulWidget {
+  const CupertinoScrollbarWidget({Key? key}) : super(key: key);
+
+  @override
+  _CupertinoScrollbarWidgetState createState() =>
+      _CupertinoScrollbarWidgetState();
+}
+
+class _CupertinoScrollbarWidgetState extends State<CupertinoScrollbarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: CupertinoScrollbar(
+        thickness: 6.0,
+        thicknessWhileDragging: 10.0,
+        radius: const Radius.circular(34.0),
+        radiusWhileDragging: Radius.zero,
+        child: ListView.builder(
+          itemCount: 50,
+          itemBuilder: (BuildContext context, int index) {
+            return Center(
+              child: Text(
+                '$index',
+                style: const TextStyle(fontSize: 30),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
