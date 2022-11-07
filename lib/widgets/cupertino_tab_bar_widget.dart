37c1810ccf6c9c92977f8b3075c98286676de9f6
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2022-11-07 11:41:28 +0600

    just update

---
+++ lib/widgets/cupertino_tab_bar_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CupertinoTabBarWidget extends StatefulWidget {
+  const CupertinoTabBarWidget({Key? key}) : super(key: key);
+
+  @override
+  _CupertinoTabBarWidgetState createState() => _CupertinoTabBarWidgetState();
+}
+
+class _CupertinoTabBarWidgetState extends State<CupertinoTabBarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
