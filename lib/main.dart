ac72eac55c206397c1c5dc9eeddf2981565bd4d2
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-05 23:03:11 +0600

    expansion panel list

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'widgets/expand_icon.dart';
+import 'package:flutter_application_1/widgets/expansion_panel_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //     primarySwatch: Colors.blue,
       //     ),
-      home: ExpandIconWidgets(),
+      home: ExpansionPanelListWidget(),
     );
   }
 }
