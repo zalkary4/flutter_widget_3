4b40c2eb46f160fd3456521d82ecc35b5e9f6a1d
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-05 23:48:01 +0600

    fade transition widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/widgets/expansion_tile_widget.dart';
+
+import 'widgets/fade_transition_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //     primarySwatch: Colors.blue,
       //     ),
-      home: ExpansionTileWidget(),
+      home: FadeTransitionWidget(),
     );
   }
 }
---
+++ lib/widgets/fade_transition_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class FadeTransitionWidget extends StatefulWidget {
+  const FadeTransitionWidget({Key? key}) : super(key: key);
+
+  @override
+  _FadeTransitionWidgetState createState() => _FadeTransitionWidgetState();
+}
+
+class _FadeTransitionWidgetState extends State<FadeTransitionWidget>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 1),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeIn,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FadeTransition(
+          opacity: _animation,
+          child: const FlutterLogo(
+            size: 300,
+          ),
+        ),
+      ),
+    );
+  }
+}
