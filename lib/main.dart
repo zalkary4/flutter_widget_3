dd66d3467074d61dd1df48164c13f4d4d00e95de
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-06 12:00:00 +0600

    validate widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/widgets/flutter_logo_with_text.dart';
+import 'package:flutter_application_1/widgets/form_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //     primarySwatch: Colors.blue,
       //     ),
-      home: FlutterLogoWithText(),
+      home: FormWidget(),
     );
   }
 }
---
+++ lib/widgets/form_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+final _formKey = GlobalKey<FormState>();
+
+class FormWidget extends StatelessWidget {
+  const FormWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Enter something';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Enter something';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Great'),
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Validate'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
