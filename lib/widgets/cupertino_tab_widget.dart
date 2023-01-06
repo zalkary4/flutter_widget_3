ec18744031375a9db752c27c5254f42477762885
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-06 23:36:14 +0600

    cupertino tab widget

---
+++ lib/widgets/cupertino_tab_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+
+class CupertinoTabWidget extends StatefulWidget {
+  const CupertinoTabWidget({Key? key}) : super(key: key);
+
+  @override
+  _CupertinoTabWidgetState createState() => _CupertinoTabWidgetState();
+}
+
+class _CupertinoTabWidgetState extends State<CupertinoTabWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+      tabBuilder: (BuildContext context, int index) {
+        return CupertinoTabView(
+          builder: (BuildContext context) {
+            return Center(
+              child: Icon(
+                  index == 0 ? CupertinoIcons.home : CupertinoIcons.settings),
+            );
+          },
+        );
+      },
+    );
+  }
+}
