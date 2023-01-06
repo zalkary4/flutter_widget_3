fb4628bd1f631fc3fde5ed3de031fdd1c8eace8c
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-06 16:21:57 +0600

    fractionaly sized boxwidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/widgets/form_widget.dart';
+
+import 'widgets/fractionaly_sized_box_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //     primarySwatch: Colors.blue,
       //     ),
-      home: FormWidget(),
+      home: FractionalySizedBoxWidget(),
     );
   }
 }
---
+++ lib/widgets/fractionaly_sized_box_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FractionalySizedBoxWidget extends StatelessWidget {
+  const FractionalySizedBoxWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FractionallySizedBox(
+        widthFactor: 0.5,
+        heightFactor: 0.9,
+        child: Container(
+          color: Colors.amberAccent,
+        ),
+      ),
+    );
+  }
+}
