8fc9d1ca510720e56f730127d9449e4ceec7bb82
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-06 11:38:27 +0600

    flutter logo with text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/widgets/flow_widget.dart';
+import 'package:flutter_application_1/widgets/flutter_logo_with_text.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //     primarySwatch: Colors.blue,
       //     ),
-      home: FlowWidget(),
+      home: FlutterLogoWithText(),
     );
   }
 }
---
+++ lib/widgets/flutter_logo_with_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class FlutterLogoWithText extends StatefulWidget {
+  const FlutterLogoWithText({Key? key}) : super(key: key);
+
+  @override
+  _FlutterLogoWithTextState createState() => _FlutterLogoWithTextState();
+}
+
+class _FlutterLogoWithTextState extends State<FlutterLogoWithText> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: FlutterLogo(
+          size: 200,
+          style: FlutterLogoStyle.stacked,
+          textColor: Colors.orange,
+        ),
+      ),
+    );
+  }
+}
