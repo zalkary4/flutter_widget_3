d9b32122310d5a25b3dee6ee4539efe6bac29031
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-15 21:21:22 +0600

    linput chip widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/long_press_draggable_widget.dart';
+import 'package:flutter_application_1/input_chip_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: LongPressDraggableWidget(),
+        body: InputChipWidget(),
       ),
     );
   }
