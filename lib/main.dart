1b913ed8c3029ef6c808b979776f65351810a083
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-15 21:07:32 +0600

    long press draggable widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/cupertino_text_field_widget.dart';
+import 'package:flutter_application_1/long_press_draggable_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +9,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: CupertinoTextFieldWidget(),
+        body: LongPressDraggableWidget(),
       ),
     );
   }
