c93e04ed13e16fc004933ab48420d13a3cfda1d7
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-15 12:30:57 +0600

    grid paper widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // title: 'Flutter Demo',
       // theme: ThemeData(
-      //     primarySwatch: Colors.blue,
-      //     ),
+      //   colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
+      //       .copyWith(background: Colors.orangeAccent),
+      // ),
       home: Scaffold(
         body: GridPaperWidget(),
       ),
