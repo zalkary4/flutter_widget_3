f91a446bb570348216e286350906c3eb31128f52
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-16 15:31:14 +0600

    list tile widget

---
+++ lib/list_tile_widget2.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ListTileWidget2 extends StatefulWidget {
+  const ListTileWidget2({Key? key}) : super(key: key);
+
+  @override
+  _ListTileWidget2State createState() => _ListTileWidget2State();
+}
+
+class _ListTileWidget2State extends State<ListTileWidget2> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListTile(
+        title: const Text('Flutter Mapp'),
+        tileColor: Colors.orangeAccent,
+        onTap: () {},
+        leading: const Icon(Icons.person),
+        trailing: const Icon(Icons.menu),
+      ),
+    );
+  }
+}
