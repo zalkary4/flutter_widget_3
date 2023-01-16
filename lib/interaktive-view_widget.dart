7a62da6c7a0d7ef8f7a09250ea9d6014564067e2
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-16 15:19:54 +0600

    interaktive viewer widget

---
+++ lib/interaktive-view_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class InteraktiveViewWidget extends StatefulWidget {
+  const InteraktiveViewWidget({Key? key}) : super(key: key);
+
+  @override
+  _InteraktiveViewWidgetState createState() => _InteraktiveViewWidgetState();
+}
+
+class _InteraktiveViewWidgetState extends State<InteraktiveViewWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orangeAccent,
+      body: InteractiveViewer(
+        boundaryMargin: const EdgeInsets.all(double.infinity),
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter Mapp'),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/input_chip_widget.dart';
+import 'package:flutter_application_1/interaktive-view_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: InputChipWidget(),
+        body: InteraktiveViewWidget(),
       ),
     );
   }
