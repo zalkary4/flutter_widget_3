88f7e2a27592f9eec4b86de4ab75b145048e3fab
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-19 19:26:06 +0600

    null

--- lib/widgets/modal_barrier_widget2.dart
+++ lib/widgets/modal_barrier_widget2.dart
@@ -37,7 +37,7 @@ class _ModalBarrierWidget2State extends State<ModalBarrierWidget2> {
                   },
                 );
               },
-              color: Colors.blue,
+              color: Colors.red,
             ),
           )
       ],
