a819de46ceb679867c30eba13a3f38ec5acf2a79
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-21 20:22:00 +0600

    image widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/ignore_pointer_widget2.dart';
+import 'package:flutter_application_1/image_widget2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: IgnorePointerWidget2(),
+        body: ImageWidget2(),
       ),
     );
   }
