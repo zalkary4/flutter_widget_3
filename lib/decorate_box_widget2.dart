9d4a53255d25a810d500f3f8c8c56f80a512aa31
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-01-22 09:36:39 +0600

    decorate box widget

---
+++ lib/decorate_box_widget2.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class DecorateBoxWidget2 extends StatefulWidget {
+  const DecorateBoxWidget2({Key? key}) : super(key: key);
+
+  @override
+  _DecorateBoxWidget2State createState() => _DecorateBoxWidget2State();
+}
+
+class _DecorateBoxWidget2State extends State<DecorateBoxWidget2> {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: [Colors.deepOrange, Colors.red],
+          ),
+        ),
+      ),
+    );
+  }
+}
